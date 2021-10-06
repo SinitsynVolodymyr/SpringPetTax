6be9205acc32ae67228585df39130922d724edb5
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-10-07 02:50:52 +0300

    Updated SecurityConfig

--- src/main/java/ua/sinitsyn/config/SecurityConfig.java
+++ src/main/java/ua/sinitsyn/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ua.sinitsyn.model.Role;
 import ua.sinitsyn.service.UserService;
 
 @Configuration
@@ -18,6 +20,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .antMatchers("/register").not().authenticated()
+                    .antMatchers("/user/**").hasRole(Role.USER.name())
+                    .antMatchers("/inspector/**").hasRole(Role.INSPECTOR.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin().permitAll()
+                .and()
+                    .logout().permitAll();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
