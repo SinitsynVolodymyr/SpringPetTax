2257df5c63839f410af03d5c582395029270fe32
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-10-07 00:44:13 +0300

    Created initializer of database

--- src/main/java/ua/sinitsyn/InitDatabase.java
+++ src/main/java/ua/sinitsyn/InitDatabase.java
@@ -1,5 +1,48 @@
 package ua.sinitsyn;
 
-public class InitDatabase {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import ua.sinitsyn.model.Role;
+import ua.sinitsyn.model.User;
+import ua.sinitsyn.service.UserService;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+public class InitDatabase implements CommandLineRunner {
+    @Autowired
+    private UserService userService;
+
+    public void run(String[] strings){
+        List<User> userList = Stream.of(
+                new User(
+                        "user@email.com"
+                        , "$2a$12$7lSB0x8eD42B8jFlbhFh5e81ydMU5bTPtL0Ah4O.wwbmhJJetTTJq"
+                        , Role.USER),
+                new User(
+                        "insp@email.com"
+                        , "$2a$12$q0s6EWp1UcXtcAPdtmXvfuGOzlYPhdvvWlzYTHGQ4nPOekQRrEJce"
+                        , Role.INSPECTOR)
+        ).collect(Collectors.toList());
+
+        if (!checkIsInitUserTable(userList)){
+            userList.stream().forEach(user -> userService.saveUser(user));
+        }
+    }
+
+    private boolean checkIsInitUserTable(List<User> userList){
+        String email = userList.stream().findFirst().get().getEmail();
+        try {
+            userService.loadUserByUsername(email);
+        }catch (UsernameNotFoundException e){
+            return false;
+        }
+        return true;
+    }
 
 }
