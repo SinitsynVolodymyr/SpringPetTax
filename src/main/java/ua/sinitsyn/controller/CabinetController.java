d9f4d1b7c9216e265712aaa59e3bf36d4cf59516
Sinitsyn Volodymyr <SinitsynVolodymyr@gmail.com>
2021-10-08 15:30:53 +0300

    Added the log

--- src/main/java/ua/sinitsyn/controller/CabinetController.java
+++ src/main/java/ua/sinitsyn/controller/CabinetController.java
@@ -1,6 +1,8 @@
 package ua.sinitsyn.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Marker;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import ua.sinitsyn.model.Role;
 import ua.sinitsyn.model.User;
 
+@Slf4j
 @Controller
 @RequestMapping("cabinet")
 public class CabinetController {
@@ -17,10 +20,13 @@ public class CabinetController {
         User user = (User) auth.getPrincipal();
 
         if (user.getRole().equals(Role.USER)){
+            log.info("Cabinet redirect to /cabinet/user");
             return "redirect:/cabinet/user";
         }else if (user.getRole().equals(Role.INSPECTOR)){
+            log.info("Cabinet redirect to /cabinet/inspector");
             return "redirect:/cabinet/inspector";
         }
+        log.error("Not equals role in user");
         return "redirect:/";
     }
 
